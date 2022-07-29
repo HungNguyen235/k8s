7ed8bcc74a503a6c2785a5504162b6db64109522
hung nguyen <hungnguyen23052000@gmail.com>
2022-07-29 07:09:45 +0000

    change file

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student from vietnam team devops
+            Hi Bala
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
